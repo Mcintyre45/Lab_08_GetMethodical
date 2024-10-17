8de84ffe38ecd58ffc30d772b01436891e85c4ee
Ethan Mcintyre <mcintyea@mail.uc.edu>
2024-10-17 12:53:32 -0400

    Method 1

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
+
 public class SafeInput
 {
+    /**
+     * gets a string from the user using the console and
+     * it must be at least one character in length
+     *
+     * @param pipe a scanner used to get the input
+     * @param prompt the prompt that tells the user what to enter
+     * @return a String of at least one character in length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retVal = "";
 
+        do
+        {
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+
+            if(retVal.isEmpty()) {
+                System.out.println("You must enter at least one character!");
+            }
+
+        }while(retVal.isEmpty());
+
+        return retVal;
+    }
 }
+
